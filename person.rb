4396503036a5f6513b1c2388bd2944a8725548fe
shinhyo26okazaki <shibo6937@gmail.com>
2023-03-07 18:20:47 -0400

    Fix linters

--- person.rb
+++ person.rb
@@ -1,10 +1,13 @@
 require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super
     @id = Random.rand(1..1000)
     @name = name
     @age = age
